34e50ec5e232fe5fa3ab8e8e5870dfa374e1bc26
Diana Kozachenko <kozachenkodiana03@mail.ru>
2025-02-28 17:50:26 +0100

    Add tests for cold observables

--- src/fork-join-settled.spec.ts
+++ src/fork-join-settled.spec.ts
@@ -4,7 +4,7 @@ import { ColdObservable } from 'jest-marbles/dist/typings/src/rxjs/cold-observab
 import { forkJoinSettled } from './fork-join-settled';
 import { PromiseAllItem, PromiseAllStatus } from './types';
 
-function generateResultValue(inputValues: any[]): PromiseAllItem<any>[] {
+function generateOutputValue(inputValues: any[]): PromiseAllItem<any>[] {
   return inputValues.map((item: any) => {
     if (item instanceof Error) {
       return {
@@ -21,26 +21,21 @@ function generateResultValue(inputValues: any[]): PromiseAllItem<any>[] {
 }
 
 function generateSingleEmittedObservables(inputValues: (any | Error)[], frames: string[] = []): ColdObservable[] {
-  return inputValues.map((item: any, index: number) => {
-    if (item instanceof Error) {
-      return cold(`${frames[index] ?? ''}#`, null, item);
+  return inputValues.map((inputValue: any | Error, index: number) => {
+    if (inputValue instanceof Error) {
+      return cold(`${frames[index] ?? ''}#`, null, inputValue);
     }
-    return cold(`${frames[index] ?? ''}a|`, { a: item });
+    return cold(`${frames[index] ?? ''}a|`, { a: inputValue });
   });
 }
 
-  // combined valid and error values (2-5)
-// observable with multiple emitted values
-  // multiple valid values (2-5)
-  // multiple valid values with delay
-  // multiple error values (2-5)
-  // combined valid and error values (2-5)
 describe('fork-join-settled', () => {
   describe('cold observables', () => {
     test('if sources are empty it should complete immediately', () => {
-      const result = forkJoinSettled([]);
+      const actualObservable = forkJoinSettled([]);
+      const expectedObservable = cold('|');
 
-      expect(result).toBeObservable(cold('|'));
+      expect(actualObservable).toBeObservable(expectedObservable);
     });
 
     describe('single emitted value', () => {
@@ -51,15 +46,13 @@ describe('fork-join-settled', () => {
           [[5, 'test', true]],
           [[5, 'test', true, { prop: 'value' }]],
           [[5, 'test', true, { prop: 'value' }, [1, 2, 3, 4]]],
-        ])('if sources (%j) are completed without errors it should complete with fulfield items', (inputValues: any[]) => {
-          const sources = generateSingleEmittedObservables(inputValues)
-          const expectedResult = generateResultValue(inputValues);
+        ])('if sources (%j) are completed successfully it should complete with fulfield items', (inputValues: any[]) => {
+          const sources = generateSingleEmittedObservables(inputValues);
 
-          const result = forkJoinSettled(sources);
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold('-(a|)', { a: generateOutputValue(inputValues) });
 
-          expect(result).toBeObservable(cold('-(a|)', {
-            a: expectedResult
-          }));
+          expect(actualObservable).toBeObservable(expectedObservable);
         });
 
         test('if sources are completed without errors with same delay it should complete with fulfield items after this delay', () => {
@@ -67,13 +60,11 @@ describe('fork-join-settled', () => {
           const inputValues = [5, 'test', true, { prop: 'value' }, [1, 2, 3, 4]];
           const frames = Array(inputValues.length).fill(FRAME_LENGTH);
           const sources = generateSingleEmittedObservables(inputValues, frames);
-          const expectedResult = generateResultValue(inputValues);
 
-          const result = forkJoinSettled(sources);
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold(`${FRAME_LENGTH}-(a|)`, { a: generateOutputValue(inputValues) });
 
-          expect(result).toBeObservable(cold(`${FRAME_LENGTH}-(a|)`, {
-            a: expectedResult
-          }));
+          expect(actualObservable).toBeObservable(expectedObservable);
         });
 
         test('if sources are completed without errors with different delay it should complete with fulfield items after longest delay', () => {
@@ -81,13 +72,11 @@ describe('fork-join-settled', () => {
           const frames = ['', '--', '-', '', '---'];
           const longestFrameLength = Math.max(...frames.map(frame => frame.length));
           const sources = generateSingleEmittedObservables(inputValues, frames);
-          const expectedResult = generateResultValue(inputValues);
 
-          const result = forkJoinSettled(sources);
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold(`${'-'.repeat(longestFrameLength)}-(a|)`, { a: generateOutputValue(inputValues) });
 
-          expect(result).toBeObservable(cold(`${'-'.repeat(longestFrameLength)}-(a|)`, {
-            a: expectedResult
-          }));
+          expect(actualObservable).toBeObservable(expectedObservable);
         });
       });
 
@@ -97,14 +86,12 @@ describe('fork-join-settled', () => {
           [[new Error('error 1'), new Error('error 2')]],
           [[new Error('error 1'), new Error('error 2'), new Error('error 3')]],
         ])('if sources (%p) are with errors it should complete with rejected items', (inputValues: Error[]) => {
-          const sources = generateSingleEmittedObservables(inputValues)
-          const expectedResult = generateResultValue(inputValues);
+          const sources = generateSingleEmittedObservables(inputValues);
 
-          const result = forkJoinSettled(sources);
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold('(a|)', { a: generateOutputValue(inputValues) });
 
-          expect(result).toBeObservable(cold('(a|)', {
-            a: expectedResult
-          }));
+          expect(actualObservable).toBeObservable(expectedObservable);
         });
 
         test('if sources are with errors with same delay it should complete with rejected items after this delay', () => {
@@ -112,13 +99,11 @@ describe('fork-join-settled', () => {
           const inputValues = [new Error('error 1'), new Error('error 2'), new Error('error 3')];
           const frames = Array(inputValues.length).fill(FRAME_LENGTH);
           const sources = generateSingleEmittedObservables(inputValues, frames);
-          const expectedResult = generateResultValue(inputValues);
 
-          const result = forkJoinSettled(sources);
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold(`${FRAME_LENGTH}(a|)`, { a: generateOutputValue(inputValues) });
 
-          expect(result).toBeObservable(cold(`${FRAME_LENGTH}(a|)`, {
-            a: expectedResult
-          }));
+          expect(actualObservable).toBeObservable(expectedObservable);
         });
 
         test('if sources are with errors with different delay it should complete with rejected items after longest delay', () => {
@@ -126,16 +111,86 @@ describe('fork-join-settled', () => {
           const frames = ['', '--', '-'];
           const longestFrameLength = Math.max(...frames.map(frame => frame.length));
           const sources = generateSingleEmittedObservables(inputValues, frames);
-          const expectedResult = generateResultValue(inputValues);
 
-          const result = forkJoinSettled(sources);
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold(`${'-'.repeat(longestFrameLength)}(a|)`, { a: generateOutputValue(inputValues) });
 
-          expect(result).toBeObservable(cold(`${'-'.repeat(longestFrameLength)}(a|)`, {
-            a: expectedResult
-          }));
+          expect(actualObservable).toBeObservable(expectedObservable);
         });
       })
-    });
-  })
-});
 
+      describe('combined cases', () => {
+        test('if sources are combined (completed successfully and with errors both) it should complete with combined items', () => {
+          const inputValues = [5, 'test', true, new Error('error 1'), { prop: 'value' }, [1, 2, 3, 4], new Error('error 2')];
+          const sources = generateSingleEmittedObservables(inputValues);
+
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold(`-(a|)`, { a: generateOutputValue(inputValues) });
+
+          expect(actualObservable).toBeObservable(expectedObservable);
+        });
+      });
+    })
+
+    describe('multiple emitted values', () => {
+      describe('completed successfully', () => {
+        test('if sources are completed successfully it should complete with lastest fulfield items', () => {
+          const obs1 = cold(`-a-|`, { a: 5 });
+          const obs2 = cold(`-a-b|`, { a: 5, b: 'test' });
+          const obs3 = cold(`-a-b-c--|`, { a: 5, b: 'test', c: true });
+          const obs4 = cold(`a|`, { a: { prop: 'value' } });
+          const obs5 = cold(`a---b|`, { a: { prop: 'value' }, b: [1, 2, 3, 4] });
+          const sources = [obs1, obs2, obs3, obs4, obs5];
+
+          const longestMarbles = Math.max(...sources.map(obs => obs.marbles.replace('|', '').length));
+          const latestValues = [5, 'test', true, { prop: 'value' }, [1, 2, 3, 4]];
+
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold(`${'-'.repeat(longestMarbles)}(a|)`, { a: generateOutputValue(latestValues) });
+
+          expect(actualObservable).toBeObservable(expectedObservable);
+        });
+      });
+
+
+      describe('with errors', () => {
+        test('if sources are completed with errors it should complete with lastest rejected items', () => {
+          const obs1 = cold(`-a-#`, { a: 5 }, new Error('error 1'));
+          const obs2 = cold(`-a-b-#`, { a: 5, b: 'test' }, new Error('error 2'));
+          const obs3 = cold(`-a-b-c--#`, { a: 5, b: 'test', c: true }, new Error('error 3'));
+          const obs4 = cold(`a#`, { a: { prop: 'value' } }, new Error('error 4'));
+          const obs5 = cold(`a---b#`, { a: { prop: 'value' }, b: [1, 2, 3, 4] }, new Error('error 5'));
+          const sources = [obs1, obs2, obs3, obs4, obs5];
+
+          const longestMarbles = Math.max(...sources.map(obs => obs.marbles.replace('#', '').length));
+          const latestValues = [new Error('error 1'), new Error('error 2'), new Error('error 3'), new Error('error 4'), new Error('error 5')];
+
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold(`${'-'.repeat(longestMarbles)}(a|)`, { a: generateOutputValue(latestValues) });
+
+          expect(actualObservable).toBeObservable(expectedObservable);
+        });
+      });
+
+      describe('combined cases', () => {
+        test('if sources are combined (completed successfully and with errors both) it should complete with lastest rejected items (fulfield and rejected respectively)', () => {
+          const obs1 = cold(`-a--|`, { a: 5 });
+          const obs2 = cold(`-a-b|`, { a: 5, b: 'test' });
+          const obs3 = cold(`-a-b--#`, { a: 5, b: 'test' }, new Error('error 1'));
+          const obs4 = cold(`a|`, { a: { prop: 'value' } });
+          const obs5 = cold(`a-b-c|`, { a: { prop: 'value' }, b: true, c: [1, 2, 3, 4] });
+          const obs6 = cold(`a-b-#`, { a: { prop: 'value' }, b: [1, 2, 3, 4] }, new Error('error 2'));
+          const sources = [obs1, obs2, obs3, obs4, obs5, obs6];
+
+          const longestMarbles = Math.max(...sources.map(obs => obs.marbles.replace('|', '').replace('#', '').length));
+          const latestValues = [5, 'test', new Error('error 1'), { prop: 'value' }, [1, 2, 3, 4], new Error('error 2')];
+
+          const actualObservable = forkJoinSettled(sources);
+          const expectedObservable = cold(`${'-'.repeat(longestMarbles)}(a|)`, { a: generateOutputValue(latestValues) });
+
+          expect(actualObservable).toBeObservable(expectedObservable);
+        });
+      });
+    });
+  });
+});
